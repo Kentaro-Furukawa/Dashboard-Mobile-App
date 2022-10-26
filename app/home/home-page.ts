010b33ec8fecf676b9d36e077ac4d9800434d2a9
Ken <info@ken89.com>
2022-10-27 08:13:07 +0900

    add onTapAdd func

--- app/home/home-page.ts
+++ app/home/home-page.ts
@@ -1,8 +1,14 @@
 import { Button } from '@nativescript/core'
 import { HomeViewModel } from './home-view-model'
 
-export function onTap(args) {
+export function onTapEdit(args) {
     const button = args.object as Button
     // execute your custom logic here...
     console.log("tap edit")
+  }
+  
+export function onTapAdd(args) {
+    const button = args.object as Button
+    // execute your custom logic here...
+    console.log("tap add")
   }
